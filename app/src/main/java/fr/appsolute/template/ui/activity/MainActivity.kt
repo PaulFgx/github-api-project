803f563293fcf3e947feff3264cdc55c8d1e1a4f
PaulFgx <pfourgoux@live.fr>
2020-01-12 09:23:21 +0100

    add xml files and nav graph

--- app/src/main/java/fr/appsolute/template/ui/activity/MainActivity.kt
+++ app/src/main/java/fr/appsolute/template/ui/activity/MainActivity.kt
@@ -2,12 +2,29 @@ package fr.appsolute.template.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import fr.appsolute.template.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolBar()
+    }
+
+    override fun onNavigateUp(): Boolean {
+        // We just say to the activity that its back stack will manage by the NavController
+        return findNavController(R.id.main_fragment_container).navigateUp()
+    }
+
+    /**
+     * Init the ToolBar
+     */
+    private fun initToolBar() {
+        setSupportActionBar(main_tool_bar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        main_tool_bar.setNavigationOnClickListener { onNavigateUp() }
     }
 }
---
+++ app/src/main/java/fr/appsolute/template/ui/fragment/UserListFragment.kt
@@ -0,0 +1,61 @@
+package fr.appsolute.template.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import fr.appsolute.template.R
+import fr.appsolute.template.data.model.User
+import fr.appsolute.template.ui.activity.MainActivity
+import fr.appsolute.template.ui.adapter.UserAdapter
+import fr.appsolute.template.ui.viewmodel.UserViewModel
+import fr.appsolute.template.ui.widget.holder.OnUserClickListener
+import kotlinx.android.synthetic.main.fragment_user_list.view.*
+
+class UserListFragment : Fragment(),
+    OnUserClickListener {
+
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var userAdapter: UserAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.run {
+            userViewModel = ViewModelProvider(this, UserViewModel).get()
+        } ?: throw IllegalStateException("Invalid Activity")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? MainActivity)?.supportActionBar?.apply {
+            this.setTitle(R.string.profils)
+            this.setDisplayHomeAsUpEnabled(false)
+        }
+        // We need to inject the OnUserClickListener in the constructor of the adapter
+        userAdapter = UserAdapter(this)
+        view.user_list_recycler_view.apply {
+            adapter = userAdapter
+            if (itemDecorationCount == 0) addItemDecoration(UserAdapter.OffsetDecoration())
+        }
+
+        userViewModel.getAllUsers {
+            userAdapter.submitList(it)
+        }
+    }
+
+    // Implementation of OnUserClickListener
+    override fun invoke(view: View, user: User) {
+        //
+    }
+}
