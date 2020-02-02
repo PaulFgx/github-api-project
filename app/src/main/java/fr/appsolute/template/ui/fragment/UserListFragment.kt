f6a5956c2506847ca5ad3717d5d0ea42a952b4d5
PaulFgx <pfourgoux@live.fr>
2020-02-02 16:05:23 +0100

    update nav graph

--- app/src/main/java/fr/appsolute/template/ui/fragment/UserListFragment.kt
+++ app/src/main/java/fr/appsolute/template/ui/fragment/UserListFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.navigation.fragment.findNavController
 import fr.appsolute.template.R
 import fr.appsolute.template.data.model.User
 import fr.appsolute.template.ui.activity.MainActivity
@@ -56,6 +58,11 @@ class UserListFragment : Fragment(),
 
     // Implementation of OnUserClickListener
     override fun invoke(view: View, user: User) {
-        //
+        findNavController().navigate(
+            R.id.action_user_list_fragment_to_user_details_fragment,
+            bundleOf(
+                UserDetailsFragment.ARG_USER_URL_KEY to user.url
+            )
+        )
     }
 }
