f4c64dfc60179bca558c4081c1e7b8a4ef347778
PaulFgx <pfourgoux@live.fr>
2020-03-15 15:31:01 +0100

    update application class

--- app/src/main/java/fr/paulfgx/githubproject/GitHubApiApplication.kt
+++ app/src/main/java/fr/paulfgx/githubproject/GitHubApiApplication.kt
@@ -1,6 +1,7 @@
 package fr.paulfgx.githubproject
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import fr.paulfgx.githubproject.data.database.DatabaseManager
 
 /**
@@ -8,9 +9,12 @@ import fr.paulfgx.githubproject.data.database.DatabaseManager
  */
 class GitHubApiApplication : Application() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate() {
         super.onCreate()
         initDatabase()
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 
     // Init the database access
